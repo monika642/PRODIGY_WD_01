651bbb85be9e27d59475347f40c24b1f9f898050
monika642 <mnkkasi@gmail.com>
2025-09-15 19:59:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar color change on scroll
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
